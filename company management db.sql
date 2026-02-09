1ad23ae5ca041208647174b115568095a36f2d31
Basantakhanal <basanta4965@gmail.com>
2026-02-09 16:25:39 +0545

    completed

--- company management db.sql
+++ company management db.sql
@@ -38,31 +38,22 @@ drop table projects;
 drop table employee_projects;
 INSERT INTO departments (dept_id,dept_name,location)
  VALUES
-(1, ' Human Resources Department', 'Building A'),
-(2, 'IT Department', 'Building B'),
-(3, 'Finance Department', 'Building C'),
-(4, 'Web Department', 'Building d');
+(1, ' Human Resources Department', 'Building A');
+
 INSERT INTO employees (emp_id,emp_name,email,contact,position,salary,dept_id)
 VALUES
-(101, 'Ramesh Thapa', 'ramesh@company.com', '9841000001', 'Manager', 60000.00, 1),
-(102, 'Sita Sharma', 'sita@company.com', '9841000002', 'Developer', 45000.00, 2),
-(103, 'Binod Singh', 'binod@company.com', '9841000003', 'Accountant', 40000.00, 3),
-(104, 'Priya Lama', 'priya@company.com', '9841000004', 'HR Assistant', 35000.00, 1),
-(105, 'Saugat Thakuri','saugat@company.com','9841202365','Developer',45000,4);
+(1, 'Ramesh Thapa', 'ramesh@company.com', '9841000001', 'Manager', 60000.00, 1);
+
 INSERT INTO projects (project_id,project_name,start_date,end_date,budget)
 VALUES
-(201, 'Web Portal System', '2023-01-01', '2023-06-01', 200000.00),
-(202, 'Mobile App Development', '2023-03-05', NULL, 300000.00),
-(203, 'Payroll Automation', '2022-10-10', '2023-02-15', 150000.00),
-(204, 'Animation', '2020-9-11', '2023-06-19', 250000.00),
-(205, 'Route Hive', '2024-10-28', NULL, 150000.00);
+(1, 'Web Portal System', '2023-01-01', '2023-06-01', 200000.00);
+
 INSERT INTO employee_projects (ep_id,emp_id,project_id,role,assigned_date)
 VALUES
-(1, 102, 201, 'Backend Developer', '2023-01-02'),
-(2, 102, 202, 'Mobile Developer', '2023-03-06'),
-(3, 101, 203, 'Project Manager', '2022-10-11'),
-(4, 103, 203, 'Finance Analyst', '2022-10-12'),
-(5, 104, 203, 'Frontend Developer', '2024-5-4');
+(1, 102, 201, 'Backend Developer', '2023-01-02');
+
+select *from employee_projects;
+select*from projects;
 SELECT emp_name, email,contact
 FROM employees
 WHERE salary < 809999;
@@ -99,10 +90,9 @@ select emp_id as id from employees;
 select employees.emp_name,employee_projects.assigned_date
 from employees
 join employee_projects on employees.emp_id=employee_projects.emp_id;
-SELECT emp_name FROM employees
-EXCEPT;
-SELECT employees.emp_name
-FROM employees
+
+select employees.emp_name,employee_projects.ep_id
+from employees
 JOIN employee_projects
 ON employees.emp_id = employee_projects.emp_id;
 
@@ -125,81 +115,8 @@ ON employee_projects.emp_id = employees.emp_id
 INNER JOIN projects
 ON employee_projects.project_id = projects.project_id;
 show tables;
+SET SQL_SAFE_UPDATES = 0;
 
-
-
--- normalization show
-
-CREATE TABLE employees_unf (
-    emp_id INT PRIMARY KEY,
-    emp_name VARCHAR(100),
-    email VARCHAR(100),
-    contact VARCHAR(100),   
-    position VARCHAR(50),
-    salary DECIMAL(10,2),
-    dept_info VARCHAR(100)  
-);
-INSERT INTO employees_unf
-(emp_id, emp_name, email, contact, position, salary, dept_info)
-VALUES
-(989, "Suman Giri", "suman12@gmail.com", "9841202156,985212345", "Developer", 55000, "1-IT"),
-(101, "Ramesh Thapa", "ramesh@company.com", "9841000001,9745693120", "Manager", 60000, "1-IT");
-select * from employees_unf;
--- CONVERTING INTO 1 NF
-CREATE TABLE employees_1nf (
-    emp_id INT PRIMARY KEY,
-    emp_name VARCHAR(100),
-    email VARCHAR(100),
-    contact1 VARCHAR(100), 
-    contact2 varchar(100),
-    position VARCHAR(50),
-    salary DECIMAL(10,2),
-    dept_info VARCHAR(100)  
-);
-drop table employees_1nf;
-select * from employees_1nf;
-INSERT INTO employees_1nf VALUES
-(989, "Suman Giri", "suman12@gmail.com", "9841202156","985212345", "Developer", 55000, 1),
-(101, "Ramesh Thapa", "ramesh@company.com", "9841000001","9745693120", "Manager", 60000, 1);
-
-
--- CONVERTING INTO 2 NF
-CREATE TABLE employees_2nf (
-    emp_id INT PRIMARY KEY,
-    emp_name VARCHAR(100),
-    email VARCHAR(100),
-    position VARCHAR(50),
-    salary DECIMAL(10,2),
-    dept_id INT
-);
-drop table employees_2nf;
-drop table employee_contacts_2nf;
-CREATE TABLE employee_contacts_2nf (
-    emp_id INT,
-    contact1 VARCHAR(15),
-    contact2 varchar(15)
-);
-INSERT INTO employees_2nf VALUES
-(989, "Suman Giri", "suman12@gmail.com", "Developer", 55000, 1),
-(101, "Ramesh Thapa", "ramesh@company.com", "Manager", 60000, 1);
-
-INSERT INTO employee_contacts_2nf VALUES
-(989, "9841202156","985212345"),
-(101, "9841000001","9745693120");
-
-select*from employee_contacts_2nf;
-select * from employees_2nf;
--- CONVERTING IN 3 NF
-CREATE TABLE departments_3nf (
-    dept_id INT PRIMARY KEY,
-    dept_name VARCHAR(100)
-);
-INSERT INTO departments_3nf VALUES
-(1, "IT"),
-(2, "Finance"),
-(3, "HR");
-select * from  departments_3nf;
-select * from employees;
 -- transcation 
 START TRANSACTION;
 INSERT INTO employees(emp_id, emp_name, email, contact, position, salary, dept_id)
@@ -216,7 +133,136 @@ SET salary = salary + 500000
 WHERE emp_id = 103;
 COMMIT;
 
+select* from departments where dept_id=1;
 
+-- unf
+
+CREATE TABLE employees_unfs (
+    emp_id INT,
+    emp_name VARCHAR(100),
+    email VARCHAR(100),
+    contact VARCHAR(100),
+    position VARCHAR(50),
+    salary DECIMAL(10,2),
+    dept_info VARCHAR(100)
+);
+
+INSERT INTO employees_unfs VALUES
+(989, "Suman Giri", "suman12@gmail.com","9841202156,985212345", "Developer", 55000, "1-IT"),
+(101, "Ramesh Thapa", "ramesh@company.com","9841000001,9745693120", "Manager", 60000, "1-IT");
+
+SELECT * FROM employees_unfs;
+
+-- 1 nf
+
+CREATE TABLE employees_1nfs (
+    emp_id INT,
+    emp_name VARCHAR(100),
+    email VARCHAR(100),
+    contact1 VARCHAR(15),
+    contact2 VARCHAR(15),
+    position VARCHAR(50),
+    salary DECIMAL(10,2),
+    dept_info VARCHAR(100)
+);
+
+INSERT INTO employees_1nfs VALUES
+(989, "Suman Giri", "suman12@gmail.com",
+ "9841202156", "985212345", "Developer", 55000, "1-IT"),
+
+(101, "Ramesh Thapa", "ramesh@company.com",
+ "9841000001", "9745693120", "Manager", 60000, "1-IT");
+
+SELECT * FROM employees_1nfs;
+
+-- 2nf
+
+CREATE TABLE employees_2nfs (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(100),
+    email VARCHAR(100),
+    position VARCHAR(50),
+    salary DECIMAL(10,2),
+    dept_id INT
+);
+
+CREATE TABLE employee_contacts_2nfs (
+    emp_id INT PRIMARY KEY,
+    contact1 VARCHAR(15),
+    contact2 VARCHAR(15)
+);
+
+INSERT INTO employees_2nfs VALUES
+(989, "Suman Giri", "suman12@gmail.com", "Developer", 55000, 1),
+(101, "Ramesh Thapa", "ramesh@company.com", "Manager", 60000, 1);
+
+INSERT INTO employee_contacts_2nfs VALUES
+(989, "9841202156", "985212345"),
+(101, "9841000001", "9745693120");
+
+SELECT * FROM employees_2nfs;
+SELECT * FROM employee_contacts_2nfs;
+
+
+-- 3-nf
+
+CREATE TABLE departments_3nfs (
+    dept_id INT PRIMARY KEY,
+    dept_name VARCHAR(100)
+);
+
+INSERT INTO departments_3nfs VALUES
+(1, "IT"),
+(2, "Finance"),
+(3, "HR");
+
+
+CREATE TABLE employees_3nfs (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(100),
+    email VARCHAR(100),
+    position VARCHAR(50),
+    salary DECIMAL(10,2),
+    dept_id INT,
+    FOREIGN KEY (dept_id) REFERENCES departments_3nfs(dept_id)
+);
+
+INSERT INTO employees_3nfs VALUES
+(989, "Suman Giri", "suman12@gmail.com", "Developer", 55000, 1),
+(101, "Ramesh Thapa", "ramesh@company.com", "Manager", 60000, 1);
+
+
+CREATE TABLE employee_contacts_3nfs (
+    emp_id INT,
+    contact_number VARCHAR(15),
+    PRIMARY KEY(emp_id, contact_number),
+    FOREIGN KEY (emp_id) REFERENCES employees_3nfs(emp_id)
+);
+
+INSERT INTO employee_contacts_3nfs VALUES
+(989, "9841202156"),
+(989, "985212345"),
+(101, "9841000001"),
+(101, "9745693120");
+
+
+SELECT * FROM departments_3nfs;
+SELECT * FROM employees_3nfs;
+SELECT * FROM employee_contacts_3nfs;
+
+show tables;
+select departments.dept_name,employees.emp_name
+from departments
+join employees on departments.dept_id=employees.emp_id;
+
+update employees set emp_id=1 where 
+emp_name="rameshthapa";
+
+select d.dept_name,e.emp_name,p.project_name
+from departments d
+inner join employees e
+inner join projects p 
+join employees on d.dept_id=e.emp_id=p.project_id;
 
 
 
